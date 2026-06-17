638e04f0b57b9ee6b9f99d4f44253ef899290132
nithin-code-web <nithinbudime@gmail.com>
2026-06-17 17:11:53 +0530

    add gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,14 +10,15 @@ import { PatientModule } from './patient/patient.module';
 import { DoctorProfile } from './doctor/entities/doctor-profile.entity';
 import { PatientProfile } from './patient/entities/patient-profile.entity';
 
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
       port: 5432,
-      username: 'postgres',
-      password: 'Nithin@2003',
+      username: 'db_name',
+      password: 'password',
       database: 'schedula',
       entities: [
         User,
